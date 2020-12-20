3cfc9f4e26ecd79a267b980db6730d0d99d6ec66
janewysoki <jane.wysoki@gmail.com>
2020-12-20 16:41:50 -0500

    commit tree d1c3bdb89d2be326e172531a43d21a9b8f171dc4

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arrayObjs) {
+  record.find(function (year, result){
+    if (result === "W") 
+      return year
+  })
+  
+}
